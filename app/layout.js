1381ab90e33eaa89f86f2f63c90337551057ee26
mariossantorinios1-cell <marios.santorinios1@gmail.com>
2025-11-18 08:00:24 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
 import "./globals.css";
-import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 export const metadata = {
   title: "AllNow",
-  description: "The ultimate social + video + gaming platform",
+  description: "All-in-one platform",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">
-        <Header />
-        <main className="pt-16">{children}</main>
+      <body className="flex bg-black text-white">
+        <Sidebar />
+        <main className="ml-20 sm:ml-64 w-full p-4">{children}</main>
       </body>
     </html>
   );
