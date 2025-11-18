718432a6a216fa91f2fb81f625d6f567a2188a78
mariossantorinios1-cell <marios.santorinios1@gmail.com>
2025-11-18 07:38:19 +0200

    Update page.js

--- app/explore/page.js
+++ app/explore/page.js
@@ -1,17 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-
 export default function Explore() {
-  const [saved, setSaved] = useState([])
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const data = localStorage.getItem('savedExplore')
-      if (data) setSaved(JSON.parse(data))
-    }
-  }, [])
-
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold">Explore</h1>
