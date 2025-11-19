0c0e3e74f975f97ccd3b71388d9e91a8111bc90c
mariossantorinios1-cell <marios.santorinios1@gmail.com>
2025-11-19 05:38:22 +0200

    Create Navbar.js

---
+++ app/components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="w-full h-16 bg-zinc-900 border-b border-zinc-800 flex items-center px-4">
+      <h1 className="text-xl font-bold">AllNow</h1>
+    </div>
+  );
+}
