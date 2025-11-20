e03a19317ea469791a3b637f43a09777a6459f31
mariossantorinios1-cell <marios.santorinios1@gmail.com>
2025-11-20 08:31:05 +0200

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,65 +1,17 @@
 "use client";
-import { FaBell, FaUserCircle, FaSearch, FaVideo } from "react-icons/fa";
+
+import { FaHome, FaSearch, FaUser } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <div style={{
-      height: "60px",
-      background: "#000",
-      color: "#fff",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "space-between",
-      padding: "0 20px",
-      position: "fixed",
-      top: 0,
-      left: 0,
-      right: 0,
-      zIndex: 100,
-      borderBottom: "1px solid #222"
-    }}>
+    <nav className="w-full h-16 bg-black text-white flex items-center justify-between px-6">
+      <div className="text-xl font-bold">AllNow</div>
 
-      {/* LOGO */}
-      <div style={{ fontSize: "22px", fontWeight: "bold", letterSpacing: "1px" }}>
-        ALLNOW
+      <div className="flex items-center gap-6 text-lg">
+        <FaHome className="cursor-pointer hover:text-gray-300" />
+        <FaSearch className="cursor-pointer hover:text-gray-300" />
+        <FaUser className="cursor-pointer hover:text-gray-300" />
       </div>
-
-      {/* SEARCH BAR */}
-      <div style={{
-        display: "flex",
-        alignItems: "center",
-        background: "#111",
-        padding: "8px 12px",
-        borderRadius: "30px",
-        border: "1px solid #333",
-        width: "40%"
-      }}>
-        <FaSearch style={{ marginRight: "10px" }} />
-        <input
-          type="text"
-          placeholder="Search..."
-          style={{
-            background: "transparent",
-            border: "none",
-            outline: "none",
-            color: "#fff",
-            width: "100%"
-          }}
-        />
-      </div>
-
-      {/* ICONS */}
-      <div style={{
-        display: "flex",
-        alignItems: "center",
-        gap: "20px",
-        fontSize: "20px"
-      }}>
-        <FaVideo style={{ cursor: "pointer" }} />
-        <FaBell style={{ cursor: "pointer" }} />
-        <FaUserCircle style={{ cursor: "pointer" }} />
-      </div>
-
-    </div>
+    </nav>
   );
 }
