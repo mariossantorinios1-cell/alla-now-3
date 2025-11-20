7f86d05b9aa5036d59fd4d8be8d4e35f5b53c553
mariossantorinios1-cell <marios.santorinios1@gmail.com>
2025-11-20 08:49:40 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}"
+    "./components/**/*.{js,jsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
