135a9e6ca81a4be223aff3bc41c19a7188cf306f
mariossantorinios1-cell <marios.santorinios1@gmail.com>
2025-11-21 05:52:07 +0200

    Create page.js

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function VideosPage() {
+  return (
+    <div className="text-white">
+      <h1 className="text-3xl font-bold mb-4">Videos</h1>
+      <p>Εδώ θα εμφανίζονται όλα τα βίντεο.</p>
+    </div>
+  );
+}
